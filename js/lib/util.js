2662dcfb13894e8e5572da9f7e489fe68b5c49cf
Evan Xd <itoyxd@gmail.com>
2014-03-22 17:26:50 +0800

    Add methods to click arrow keys.

--- js/lib/util.js
+++ js/lib/util.js
@@ -1,3 +1,30 @@
+'use strict';
+
+var MonkeyUtil = {
+  keycode: {
+    VK_LEFT: 37,
+    VK_UP: 38,
+    VK_RIGHT: 39,
+    VK_DOWN: 40
+  },
+
+  clickLeftArrowKey: function() {
+    sendKey(this.keycode.VK_LEFT);
+  },
+
+  clickUpArrowKey: function() {
+    sendKey(this.keycode.VK_UP);
+  },
+
+  clickRightArrowKey: function() {
+    sendKey(this.keycode.VK_RIGHT);
+  },
+
+  clickDownArrowKey: function() {
+    sendKey(this.keycode.VK_DOWN);
+  }
+};
+
 function sendKey(keyCode) {
   var keyEvent = document.createEventObject?
     document.createEventObject():
@@ -11,5 +38,5 @@ function sendKey(keyCode) {
   keyEvent.which = keyCode;
   document.dispatchEvent?
     document.dispatchEvent(keyEvent):
-    document.fireEvent('onkeydown', keyEvent); 
+    document.fireEvent('onkeydown', keyEvent);
 }
